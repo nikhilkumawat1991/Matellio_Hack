b7f513edfdebcab704bca302f72ee1b2b4ff6a31
nikhil kumawat <nikhil.kumawat@matellio.com>
2020-10-29 15:06:32 +0530

    changes in fluent conf file

--- fluentd/get_blob_data.py
+++ fluentd/get_blob_data.py
@@ -2,24 +2,27 @@ from azure.storage.blob import BlobServiceClient
 import pandas as pd
 from azure.storage.blob import BlobClient
 import json
+import re
 
 blob_name = []
 
 def main():
-    blob_service_client = BlobServiceClient.from_connection_string("DefaultEndpointsProtocol=https;AccountName=empcognitivestorage;AccountKey=2VFFnLiSbNW1ZdSWv7hU17sfh8WAf8X4p/m8J5WWvzLXJV1fYkQ3T6wXVEYYY0rYJYXhIWZWr//QUvapbfJH4g==;EndpointSuffix=core.windows.net")
+    blob_service_client = BlobServiceClient.from_connection_string("DefaultEndpointsProtocol=https;AccountName=empdetailstorage;AccountKey=XfDymEx6ZabXF7pJqtnp8i28HhqkfQ/4/2bW6M1Cfkj/HOlaTm3xxfkXeJs4xBAiUBVHH9J4ZXh4tUlx5wq49Q==;EndpointSuffix=core.windows.net")
 
-    container_client = blob_service_client.get_container_client("empdetails")
+    container_client = blob_service_client.get_container_client("empdetailsnew")
     #print(container_client.__dict__)
     blobs_list = container_client.list_blobs()
     print(blobs_list.__dict__)
     for blob in blobs_list:
         blob_name.append(blob.name)
+    print(blob_name)
 
-    blob = BlobClient.from_connection_string(conn_str="DefaultEndpointsProtocol=https;AccountName=empcognitivestorage;AccountKey=2VFFnLiSbNW1ZdSWv7hU17sfh8WAf8X4p/m8J5WWvzLXJV1fYkQ3T6wXVEYYY0rYJYXhIWZWr//QUvapbfJH4g==;EndpointSuffix=core.windows.net", container_name="empdetails", blob_name=str(blob_name[0]))
+    blob = BlobClient.from_connection_string(conn_str="DefaultEndpointsProtocol=https;AccountName=empdetailstorage;AccountKey=XfDymEx6ZabXF7pJqtnp8i28HhqkfQ/4/2bW6M1Cfkj/HOlaTm3xxfkXeJs4xBAiUBVHH9J4ZXh4tUlx5wq49Q==;EndpointSuffix=core.windows.net", container_name="empdetailsnew", blob_name=str(blob_name[0]))
     blob_data = blob.download_blob()
     byte_data = blob_data._current_content
     json_data = json.dumps(byte_data.decode('utf-8'))
-    print(json_data)
+    res = re.findall(r'\{.*?\}', json_data)
+    print(res)
 
 
 if __name__ == '__main__':
